51c0e40ff7453452b17ec84baf3ac977fdc07d9e
unknown <furfaro.romain@gmail.com>
2026-09-16 17:08:19 +0200

    ajout commentaire fizzbuzz

--- exercices/algo/algorithmes.js
+++ exercices/algo/algorithmes.js
@@ -1,3 +1,9 @@
+/**
+ * La fonction commence par une boucle qui parcourt i fois le paramètre n et sa longueur est déterminé par i <= n pour chaque occurrence/passage dans la boucle.
+ * Ce même paramètre est utilisé pour le calcul, en se servant du modulo (le reste de l'opération)
+ * pour déterminer si le résultat affichera FizzBuzz, Fizz ou Buzz. le résultat est affiché pour chaque occurence dans la boucle car le résultat est calculé + affiché dans celle-ci.
+ *
+ */
 function fizzBuzz(n) {
   for (let i = 1; i <= n; i++) {
     let result = i;
