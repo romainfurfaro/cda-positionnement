751502751cf23e108db190df7d375e6d434e004b
unknown <furfaro.romain@gmail.com>
2026-09-16 14:59:45 +0200

    exercice 3.1: fizzbuzz

---
+++ exercices/algo/algorithmes.js
@@ -0,0 +1,14 @@
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    let result = i;
+    if (i % 3 === 0 && i % 5 === 0) {
+      result = "FizzBuzz";
+    } else if (i % 3 === 0) {
+      result = "Fizz";
+    } else if (i % 5 === 0) {
+      result = "Buzz";
+    }
+    console.log(result);
+  }
+}
+fizzBuzz(15);
